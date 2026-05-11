175996d10ba1e3b4a62d38e930e82f6774f90451
cesar rebolledo <cesar.rebolledo44073@ucaldas.edu.co>
2026-05-10 20:56:58 -0500

    !

--- src/empresa/entities/empresa.entity.ts
+++ src/empresa/entities/empresa.entity.ts
@@ -13,32 +13,32 @@ import { TipoAlimento } from '../../tipo-alimento/entities/tipo-alimento.entity'
 @Entity('empresa')
 export class Empresa {
   @PrimaryGeneratedColumn()
-  id: number;
+  id?: number;
 
   @Column({ unique: true, length: 20 })
-  nit: string;
+  nit!: string;
 
   @Column({ name: 'nombre', length: 200 })
-  nombre: string;
+  nombre!: string;
 
   @Column({ length: 300 })
-  direccion: string;
+  direccion!: string;
 
   @Column({ name: 'tipo_negocio', length: 100 })
-  tipoNegocio: string;
+  tipoNegocio!: string;
 
   @Column({ length: 100 })
-  representante: string;
+  representante!: string;
 
   @OneToMany(() => PlanPsb, (planPsb) => planPsb.empresa)
-  planesPsb: PlanPsb[];
+  planesPsb!: PlanPsb[];
 
   @OneToMany(() => TipoAlimento, (tipoAlimento) => tipoAlimento.empresa)
-  tiposAlimento: TipoAlimento[];
+  tiposAlimento!: TipoAlimento[];
 
   @CreateDateColumn({ name: 'created_at' })
-  createdAt: Date;
+  createdAt!: Date;
 
   @UpdateDateColumn({ name: 'updated_at' })
-  updatedAt: Date;
+  updatedAt!: Date;
 }
