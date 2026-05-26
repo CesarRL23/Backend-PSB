c6efeb22e25d23f134eb30c0ecf2252e99f7d688
Yahir Rangel <yahirandres.rangel@gmail.com>
2026-05-25 22:21:17 -0500

    feat(tipo-alimento): implementar findAll con consulta real a la base de datos

--- src/tipo-alimento/tipo-alimento.service.ts
+++ src/tipo-alimento/tipo-alimento.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TipoAlimento } from './entities/tipo-alimento.entity';
 import { CreateTipoAlimentoDto } from './dto/create-tipo-alimento.dto';
 import { UpdateTipoAlimentoDto } from './dto/update-tipo-alimento.dto';
 
 @Injectable()
 export class TipoAlimentoService {
+  constructor(
+    @InjectRepository(TipoAlimento)
+    private readonly tipoAlimentoRepository: Repository<TipoAlimento>,
+  ) {}
+
   create(createTipoAlimentoDto: CreateTipoAlimentoDto) {
     return 'This action adds a new tipoAlimento';
   }
 
   findAll() {
-    return `This action returns all tipoAlimento`;
+    return this.tipoAlimentoRepository.find();
   }
 
   findOne(id: number) {
